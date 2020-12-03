70b879474c59659e10e3bbad452d7ec1ca1c5106
Mohanad Hilles <muhilles@gmail.com>
2020-12-04 01:45:51 +0200

    fix layouts

--- eventmie-pro/resources/views/layouts/include_css.blade.php
+++ eventmie-pro/resources/views/layouts/include_css.blade.php
@@ -4,9 +4,9 @@
 <!-- Bootstrap RTL CSS only if langauge is RTL -->
 @if(is_rtl())
 <link rel="stylesheet" href="{{ eventmie_asset('css/bootstrap-rtl.min.css') }}">
+<link rel="stylesheet" href="{{eventmie_asset('css/custom-ar.css')}}">
 @endif
-
-
-
 <!-- App CSS -->
 <link rel="stylesheet" href="{{ eventmie_asset('css/app_v1.5.css') }}">
+<link rel="stylesheet" href="{{eventmie_asset('css/custom.css')}}">
+
--- storage/framework/views/f9d7064d6b572d1ffeb5315894671af8d3f5d65b.php
+++ storage/framework/views/f9d7064d6b572d1ffeb5315894671af8d3f5d65b.php
@@ -4,10 +4,10 @@
 <!-- Bootstrap RTL CSS only if langauge is RTL -->
 <?php if(is_rtl()): ?>
 <link rel="stylesheet" href="<?php echo e(eventmie_asset('css/bootstrap-rtl.min.css')); ?>">
+<link rel="stylesheet" href="<?php echo e(eventmie_asset('css/custom-ar.css')); ?>">
 <?php endif; ?>
-
-
-
 <!-- App CSS -->
 <link rel="stylesheet" href="<?php echo e(eventmie_asset('css/app_v1.5.css')); ?>">
+<link rel="stylesheet" href="<?php echo e(eventmie_asset('css/custom.css')); ?>">
+
 <?php /**PATH /Users/mohanad/Documents/waleed/booking/eventmie-pro/src/../resources/views/layouts/include_css.blade.php ENDPATH**/ ?>
