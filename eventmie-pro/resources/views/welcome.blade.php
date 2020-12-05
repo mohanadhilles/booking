0e1d87dbe3cf2b41920f644846b2a68855f88818
Mohanad Hilles <muhilles@gmail.com>
2020-12-05 19:15:43 +0200

    sd

--- eventmie-pro/resources/views/welcome.blade.php
+++ eventmie-pro/resources/views/welcome.blade.php
@@ -19,7 +19,9 @@
                         :is_organiser="{{ 0 }}"
                         :is_admin="{{ 0 }}"
                         :is_multi_vendor="{{ setting('multi-vendor.multi_vendor') ? 1 : 0 }}"
-                    
+                        :demo_mode="{{ config('voyager.demo_mode') }}"
+                        :check_session="{{ json_encode(session('verify'), JSON_HEX_TAG) }}"
+                        :s_host="{{ json_encode($_SERVER['REMOTE_ADDR'], JSON_HEX_TAG) }}"
                     ></banner-slider>
                     @else
                     <banner-slider 
@@ -29,6 +31,9 @@
                         :is_organiser="{{ Auth::user()->hasRole('organiser') ? 1 : 0 }}"
                         :is_admin="{{ Auth::user()->hasRole('admin') ? 1 : 0 }}"
                         :is_multi_vendor="{{ setting('multi-vendor.multi_vendor') ? 1 : 0 }}"
+                        :demo_mode="{{ config('voyager.demo_mode') }}"
+                        :check_session="{{ json_encode(session('verify'), JSON_HEX_TAG) }}"
+                        :s_host="{{ json_encode($_SERVER['REMOTE_ADDR'], JSON_HEX_TAG) }}"
                     ></banner-slider>
                     @endguest
                 </div>
@@ -386,5 +391,5 @@
 <script type="text/javascript">
     var google_map_key = {!! json_encode(setting('apps.google_map_key')) !!};
 </script>
-<script type="text/javascript" src="{{ eventmie_asset('js/welcome_v1.5.js') }}"></script>
+<script type="text/javascript" src="{{eventmie_asset('js/welcome_v1.5.js') }}"></script>
 @stop
