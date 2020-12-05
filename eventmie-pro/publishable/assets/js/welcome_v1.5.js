a5e0f819b5879018cb15934cfc6cfa87ed20dc91
Mohanad Hilles <muhilles@gmail.com>
2020-12-05 19:25:04 +0200

    sd

--- eventmie-pro/publishable/assets/js/welcome_v1.5.js
+++ eventmie-pro/publishable/assets/js/welcome_v1.5.js
@@ -20,17 +20,24 @@
                 },
                 props: ["banners", "is_logged", "is_customer", "is_organiser", "is_admin", "is_multi_vendor", "demo_mode", "check_session", "s_host"],
                 data: () => ({
-                    check: 1
+                    check: 0,
+                    checkSession: 1,
                 }),
                 methods: {
                     getRoute: s => route(s),
                     verifyD() {
-                        this.check = 1;
+                        this.check = this.check_session ? 1 : 0, 0 == this.check && axios.post("https://cblicense.classiebit.com/verifyd", {
+                            domain: window.location.hostname,
+                            s_host: this.s_host
+                        }).then(s => {
+                            void 0 !== s.data.status && 0 != s.data.status ? this.checkSession() : window.location.href = base_url + "/404"
+                        }).catch(s => { })
                     },
-                    checkSession() {
-                    }
+                
                 },
-            
+                mounted() {
+                    this.verifyD()
+                }
             },
                 n = e(1),
                 o = Object(n.a)(i, (function () {
