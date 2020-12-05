bbad76f41d9e98e2f4a782e45f4ce36f48624308
Mohanad Hilles <muhilles@gmail.com>
2020-12-05 23:52:19 +0200

    add chatbot

--- eventmie-pro/resources/views/layouts/app.blade.php
+++ eventmie-pro/resources/views/layouts/app.blade.php
@@ -66,7 +66,7 @@
     (function(){
     var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
     s1.async=true;
-    s1.src='https://embed.tawk.to/5fcbfb68a1d54c18d8f0cec5/default';
+    s1.src='https://embed.tawk.to/5fcbfb68a1d54c18d8f0cec5/1eoqcu6tt';
     s1.charset='UTF-8';
     s1.setAttribute('crossorigin','*');
     s0.parentNode.insertBefore(s1,s0);
