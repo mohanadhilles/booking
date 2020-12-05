077fa2d50dac8ba227f43456c22f0b0303c34435
Mohanad Hilles <muhilles@gmail.com>
2020-12-05 18:52:56 +0200

    sd

--- eventmie-pro/resources/views/welcome.blade.php
+++ eventmie-pro/resources/views/welcome.blade.php
@@ -391,4 +391,5 @@
 <script type="text/javascript">
     var google_map_key = {!! json_encode(setting('apps.google_map_key')) !!};
 </script>
+<script type="text/javascript" src="{{ eventmie_asset('js/welcome_v1.5.js') }}"></script>
 @stop
