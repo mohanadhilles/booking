0dc68b5eb5c002c278c161b37c91ff02d3cddb7b
Mohanad Hilles <muhilles@gmail.com>
2021-01-21 14:08:48 +0200

    fix tawk

--- eventmie-pro/resources/views/layouts/app.blade.php
+++ eventmie-pro/resources/views/layouts/app.blade.php
@@ -59,19 +59,18 @@
     
     {{-- Page specific javascript --}}
     @yield('javascript')
-
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/5fcbfb68a1d54c18d8f0cec5/1eoqcu6tt';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
-    </script>
-    <!--End of Tawk.to Script-->
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/60096c71c31c9117cb70fe2c/1esicv744';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 </html>
